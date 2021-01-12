3a7c661f3e68ac0d3e291d685c599e27e690427b
mook30888 <mookkak@gmail.com>
2021-01-12 13:56:48 +0700

    gg

--- app_template.js
+++ app_template.js
@@ -1,1 +1,23 @@
 //your code here!
+document.addEventListener('DOMContentLoaded' , function() {
+    function update() {
+
+
+        const stuId = document.querySelector('#input-number').value
+        const list = document.getElementById(stuId)
+        const li = document.createElement('li')
+
+        let grade = document.createElement('span')
+        grade = document.getElementById('input-number')
+        let g = grade.options[grade.selectedIndex].text
+        let subjectID = document.createElement('span')
+        subjectID = document.querySelector('#input-number').value
+        li.append(g)
+        li.append(subjectID)
+        list.append(li)
+
+
+        return false
+    }
+    document.querySelector('button').onclick = update
+})
